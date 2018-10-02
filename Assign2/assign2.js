0308d94a5511860071f02d3dc86c463b0080d04e
Dang Chuong Pham <dangchuongpham1999@gmail.com>
2018-10-02 22:44:13 +0300

    Assign2

---
+++ Assign2/assign2.js
@@ -0,0 +1,20 @@
+//NOTICE: The added value should be in STRING type !
+$("#first").click(function(){
+    console.log("This function works!");
+    $("body").css("background-color", "red");
+});
+
+$("#second").click(function(){
+    console.log("This function works!");
+    $("body").css("background-color", "yellow");
+});
+
+$("#third").click(function(){
+    console.log("This function works!");
+    $("body").css("background-color", "green");
+});
+
+$("#fourth").click(function(){
+    console.log("This function works!");
+    $("body").css("background-color", "pink");
+});
